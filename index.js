b99da6d8692ff2a269173822ee1bdb776c500cb2
Darrian Bagley <dbagle14@gmail.com>
2022-03-25 18:43:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  function reverse(word) {
+    return word
+      .split("")
+      .reverse()
+      .join("");
+  }
+  return word === reverse(word) ? true : false;
 }
-
 /* 
   Add your pseudocode here
 */
+/**
+ * 1. Create an array of the word
+ * 2. Reverse the array
+ * 3. Create a string from the reversed array
+ * 4. Compare the reversed string to the original string
+ */
 
 /*
   Add written explanation of your solution here
 */
+/**
+ * The word is a palindrome if the reversed word is the same as the original word.
+ */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
